1b3fa147c3135cefcab329b15d8676b643df9cc3
vytorJS <86034275+Vytor-Rosa@users.noreply.github.com>
2022-09-17 11:47:02 -0300

    :frog:

--- back/router.js
+++ back/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const usuarios = require('./api/usuarios/usuarios.controller');
+const usuarios = require('../api/usuarios/usuarios.controller');
 
 router.use(express.json());
 router.use("/", usuarios);
