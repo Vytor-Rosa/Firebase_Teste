5242f1b1bcffdacff2348d32606d935f2f0f2042
vytorJS <86034275+Vytor-Rosa@users.noreply.github.com>
2022-09-17 12:03:53 -0300

    :frog:

--- back/index.js
+++ back/index.js
@@ -5,8 +5,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-// app.use('/api', router);
-
-app.use((req , res) => {res.json("reloooo")});
+app.use('/api', router);
 
 app.listen(3000, () => {console.log("Rodando na porta 3000")})
