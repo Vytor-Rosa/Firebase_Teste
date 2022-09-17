fa9fd30649915da0716d64b9e8d5aff1a3004e7b
vytorJS <86034275+Vytor-Rosa@users.noreply.github.com>
2022-09-16 23:59:45 -0300

    :frog:

--- front/senha/main.js
+++ front/senha/main.js
@@ -7,7 +7,7 @@ function pegar(){
 function cadastrar(){
     const senha = document.querySelector('#senha');
     const valorSenha = senha.value;
-    fetch('http://localhost:3000/api/usuarios', {
+    fetch('votacao-lime.vercel.app/api/usuarios', {
         method: 'POST',
         body: JSON.stringify({email: email, senha: valorSenha}),
         headers: {'Content-Type': 'application/json'}})
---
+++ main.js
@@ -0,0 +1,8 @@
+function evandro(){
+    var email = document.querySelector('#email');
+    var emailValor = email.value;
+    if(emailValor != ""){
+        localStorage.setItem('email', emailValor);
+        }
+    window.location.href("../senha/index.html");
+}
