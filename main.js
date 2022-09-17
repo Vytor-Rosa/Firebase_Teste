0c9c2a9d0d048805725a10fc847e97974cb3ea25
vytorJS <86034275+Vytor-Rosa@users.noreply.github.com>
2022-09-17 00:35:26 -0300

    :frog:

--- main.js
+++
@@ -1,8 +0,0 @@
-function evandro(){
-    var email = document.querySelector('#email');
-    var emailValor = email.value;
-    if(emailValor != ""){
-        localStorage.setItem('email', emailValor);
-        }
-    window.location.href("../senha/index.html");
-}
