068644a164523a3eb86a6530ff4aaaa6ba6a62ff
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2022-10-06 13:37:54 -0300

    Update index.js

--- back/index.js
+++ back/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('./router');
 const cors = require('cors');
-const app = express(); 
+const app = express();
 
 app.use(express.json());
 app.use(cors());
